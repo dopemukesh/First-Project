62e42b412c1d96a57bc2be13798c131e3c2c35f0
dopemukesh <dope.mukeshyadav@gmail.com>
2025-03-26 23:28:34 +0530

    member filtering feature added and some role based filter are made in api folder

--- src/Pages/Members/MemberCard.jsx
+++ src/Pages/Members/MemberCard.jsx
@@ -1,34 +1,48 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import members from "../../api/MembersData.json";
+import { NavLink } from "react-router-dom";
 
-const MemberCard = () => {
+const MemberCard = ({ filters = {} }) => {
+  const filteredMembers = members.filter((member) => {
+    // Loop through all filter keys and check if each matches
+    return Object.entries(filters).every(([key, value]) => {
+      return member[key] === value;
+    });
+  });
+
   return (
     <>
       <div className="my-8 flex gap-4 flex-wrap justify-center">
-        {members.length > 0 ? (
-          members.map(({ name, image, path, position, work }) => (
-            <NavLink to={path || "/"} key={image}>
-              <div className="w-80 flex-1 flex bg-gradient-to-tl from-white/10 via-transparent via-30% to-white/10 backdrop-blur border border-gray-200 dark:border-gray-700/50 shadow-2xl shadow-gray-300 dark:shadow-gray-950 rounded-xl p-4 hover:scale-105 transition-transform duration-500 ease-in-out">
-                <img
-                  src={image}
-                  alt={`${name} profile`}
-                  className="w-14 h-14 rounded-full"
-                />
-                <div className="w-full ml-4">
-                  <div className="w-full flex justify-between items-center">
-                    <p className="dark:text-gray-300 text-sm font-semibold">
-                      {name}
-                    </p>
-                    <p className="text-gray-500 text-[8px] font-medium p-1 bg-gray-200 dark:bg-gray-800 rounded border border-gray-300 dark:border-gray-700">
-                      {position}
-                    </p>
+        {filteredMembers.length > 0 ? (
+          filteredMembers.map(
+            ({ name, image, path, position, description, status, role }) => (
+              <NavLink to={path || "/"} key={image}>
+                <div className="w-80 flex-1 flex bg-gradient-to-tl from-white/10 via-transparent via-30% to-white/10 backdrop-blur border border-gray-200 dark:border-gray-700/50 shadow-2xl shadow-gray-300 dark:shadow-gray-950 rounded-xl p-4 hover:scale-105 transition-transform duration-500 ease-in-out">
+                  <img
+                    src={image}
+                    alt={`${name} profile`}
+                    className="w-14 h-14 rounded-full"
+                  />
+                  <div className="w-full ml-4">
+                    <div className="w-full flex justify-between items-center">
+                      <p className="dark:text-gray-300 text-sm font-semibold">
+                        {name}
+                      </p>
+                      {status && (
+                        <p className="text-white text-[8px] px-3 py-1 bg-emerald-500 rounded absolute -top-3 left-4">
+                          {status}
+                        </p>
+                      )}
+                      <p className="text-gray-500 text-[8px] font-medium p-1 bg-gray-200 dark:bg-gray-800 rounded border border-gray-300 dark:border-gray-700">
+                        {position}
+                      </p>
+                    </div>
+                    <p className="text-sm text-gray-500">{description}</p>
                   </div>
-                  <p className="text-sm text-gray-500">{work}</p>
                 </div>
-              </div>
-            </NavLink>
-          ))
+              </NavLink>
+            )
+          )
         ) : (
           <p>No contributors found.</p>
         )}
--- src/Pages/Members/Members.jsx
+++ src/Pages/Members/Members.jsx
@@ -15,14 +15,14 @@ const Members = () => {
   return (
     <>
       <Container>
-
         {/* top hero section contents */}
         <div className="py-28 h-fit flex flex-col gap-8 justify-center items-center">
           <h1 className="text-4xl lg:leading-[72px] lg:text-[64px] font-bold text-center text-gray-950 dark:text-white">
             Your Brand Deserves a Best Developer
           </h1>
           <p className="text-center text-gray-500 dark:text-gray-400">
-            We provide creativity and insight to deliver best website with best developers
+            We provide creativity and insight to deliver best website with best
+            developers
           </p>
 
           {/* cta buttons */}
@@ -34,9 +34,7 @@ const Members = () => {
             </NavLink>
 
             <NavLink to="">
-              <Button01 className="w-fit">
-                Book a Meet
-              </Button01>
+              <Button01 className="w-fit">Book a Meet</Button01>
             </NavLink>
           </div>
         </div>
@@ -46,40 +44,50 @@ const Members = () => {
       <Container className="py-14">
         <h1 className="text-3xl font-bold text-center">Website Managers</h1>
 
-        {/* members display grid */}
+        {/* members display according to role = Manager */}
         <div className="flex flex-wrap gap-4 justify-center">
-          <MemberCard />
+          <MemberCard filters={{ role: "Manager" }} />
         </div>
       </Container>
 
       {/* contributors display section */}
       <Container className="py-14">
-
         {/* members display grid */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 justify-center px-4">
           <div>
-            <p className='text-sm text-purple-500 font-semibold'>Our Contributors</p>
-            <p className='my-4 text-3xl font-semibold'>That contributed, <br /> to our website</p>
-            <p className='text-gray-500 max-w-2xl'>
-              Meet the talented individuals whose efforts and expertise have helped shape and elevate our website. Their contributions have been invaluable to our success.
+            <p className="text-sm text-purple-500 font-semibold">
+              Our Contributors
+            </p>
+            <p className="my-4 text-3xl font-semibold">
+              That contributed, <br /> to our website
+            </p>
+            <p className="text-gray-500 max-w-2xl">
+              Meet the talented individuals whose efforts and expertise have
+              helped shape and elevate our website. Their contributions have
+              been invaluable to our success.
             </p>
           </div>
 
+          {/* members display according to contributionType = Contributor */}
           <div className="px-4">
-            <MemberCard />
+            <MemberCard filters={{ contributionType: "Contributor" }} />
           </div>
         </div>
       </Container>
 
       <Container className="py-14">
-
-        {/* members display grid */}
+        {/* projects display grid */}
         <div className="grid grid-cols gap-4 px-4">
           <div>
-            <p className='text-sm text-purple-500 font-semibold'>Project Glymphs</p>
-            <p className='my-4 text-3xl font-semibold'>Here is projects, of our members</p>
-            <p className='text-gray-500 max-w-2xl'>
-              Discover the innovative projects crafted by our talented members. Each project showcases creativity, skill, and dedication.
+            <p className="text-sm text-purple-500 font-semibold">
+              Project Glymphs
+            </p>
+            <p className="my-4 text-3xl font-semibold">
+              Here is projects, of our members
+            </p>
+            <p className="text-gray-500 max-w-2xl">
+              Discover the innovative projects crafted by our talented members.
+              Each project showcases creativity, skill, and dedication.
             </p>
           </div>
 
@@ -88,11 +96,11 @@ const Members = () => {
             <div>
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
                 {projects.map((item) => (
-                  <div key={item.id} className="bento-item relative group cursor-pointer flex justify-center items-center w-full h-44 overflow-hidden border border-gray-300 dark:border-gray-800 rounded-xl hover:shadow-xl transition-all duration-300">
-                    <img
-                      src={item.image}
-                      className="h-fit scale-125"
-                    />
+                  <div
+                    key={item.id}
+                    className="bento-item relative group cursor-pointer flex justify-center items-center w-full h-44 overflow-hidden border border-gray-300 dark:border-gray-800 rounded-xl hover:shadow-xl transition-all duration-300"
+                  >
+                    <img src={item.image} className="h-fit scale-125" />
                     <div className="absolute hidden group-hover:block bg-teal-950/20 backdrop-blur-md w-full h-full justify-center items-center">
                       <div className="w-full h-full grid place-content-center">
                         <NavLink to={item.demoLink}>
