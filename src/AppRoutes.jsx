16968f28551dbe80c501df81cf482d99b0c70a16
YourUsername <mayankthawani13@gmail.com>
2025-06-23 19:05:23 +0530

    changes the routes

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -29,6 +29,7 @@ import TrainingDetails from "./Pages/Store/Trainings/Trainingdetails";
 import MainLayout from "./Components/Layout/allLayouts/MainLayout";
 import AuthLayout from "./Components/Layout/allLayouts/AuthLayout";
 import DashboardLayout from "./Components/Layout/allLayouts/DashboardLayout";
+import JobDetails from "./Pages/Career/Jobs/Job-details";
 
 export const AppRoutes = () => (
   <Routes>
@@ -67,6 +68,7 @@ export const AppRoutes = () => (
       <Route path="/book-details/:id" element={<BookDetails />} />
       <Route path="/product-details/:id" element={<ProductDetails />} />
       <Route path="/training-details/:id" element={<TrainingDetails />} />
+       <Route path="/job-details/:id" element={<JobDetails />} />
 
       {/* Catch-all */}
       <Route path="*" element={<Error404 />} />
